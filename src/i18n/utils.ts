b2851af4958957d46db998194394b9f7dcef21bb
Matias Acebal <acebalmatias2019@gmail.com>
2025-11-21 21:40:13 -0300

    i18n

---
+++ src/i18n/utils.ts
@@ -0,0 +1,9 @@
+import { ui, defaultLang } from './ui';
+
+
+
+export function useTranslations(lang: keyof typeof ui) {
+  return function t(key: keyof typeof ui[typeof defaultLang]) {
+    return ui[lang][key] || ui[defaultLang][key];
+  }
+}
